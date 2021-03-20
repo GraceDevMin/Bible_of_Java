ba3b9728a1db1fc42941d3fe11590e43ff32c9df
EunhyeMin <olivgrece@gmail.com>
2021-03-20 22:35:44 +0900

    210320TIL - Operator

---
+++ src/ch3/OperatorEx1.java
@@ -0,0 +1,16 @@
+package ch3;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++;		// i=i+1와 같은 의미. ++i;로 바꿔써도 의미는 같다
+		System.out.println(i);
+		
+		i = 5;
+		++i;
+		System.out.println();
+
+	}
+
+}
